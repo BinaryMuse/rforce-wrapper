28e77c065b8c188df37008545c88000e9129ea85
Brandon Tilley <binarymuse@binarymuse.net>
2011-02-23 15:36:26 -0800

    Document version

--- lib/rforce-wrapper/version.rb
+++ lib/rforce-wrapper/version.rb
@@ -1,6 +1,9 @@
 module RForce
   module Wrapper
+    # The current version of RForce-wrapper
     VERSION        = "0.0.1"
+    # The version of the Salesforce Web Services API the gem is
+    # currently written for
     SF_API_VERSION = "21.0"
   end
 end
